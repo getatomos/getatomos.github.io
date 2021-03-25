5d1479feed9a2bb72d3e735910b34de3b4475859
Sumin Kim <ppsm920@gmail.com>
2021-03-24 20:57:28 -0700

    addedLink

--- gatsby-starter-grayscale/public/sw.js
+++ gatsby-starter-grayscale/public/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ad750f062db275506c49.js"
+    "url": "webpack-runtime-b4de9115bf3918334b1f.js"
   },
   {
     "url": "framework-8e528b732ab2eaadb7b7.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d4e4fec0336381d63057fa7356111ddc"
+    "revision": "0273acae87dee52ed724c2e5d1d03768"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
